4e7f79949a7600813359f058865ad1bfc70a75ae
omkar1223 <omkargample@gmail.com>
2024-12-01 21:52:50 +0530

    db changed

--- BD4_Assignment1/initDB.js
+++ BD4_Assignment1/initDB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./BD4_Assignment1/database.sqlite', (err) => {
+const db = new sqlite3.Database('./database.sqlite', (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
